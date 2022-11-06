0c71764c50be1223773aca33232a01d8aca5a9e9
OFabuloso <johrod11@gmail.com>
2022-11-05 23:21:20 -0300

    commit inicial

---
+++ src/javascript/script.js
@@ -0,0 +1,34 @@
+function createGame(player1, hour, player2) {
+    return `
+    <li>
+    <img src="/src/bandeiras/icon=${player1}.svg" alt="${player1}">
+        <strong>${hour}</strong>
+    <img src="/src/bandeiras/icon=${player2}.svg" alt="${player2}"> 
+</li>
+`
+}
+let delay = 2;
+function createCard(date, day, games ){
+    delay = delay + 0.4;
+    return `
+    <div class="card" style="animation-delay: ${delay}s">
+    <h2>${date}<span>${day}</span></h2>
+    <ul>
+     ${games}  
+    </ul>
+</div>
+    `
+}
+
+document.querySelector('#cards').innerHTML = 
+               createCard('24/11', 'quinta',
+               createGame("brazil", '16:00', "serbia")) 
+               +
+               createCard('28/11', 'segunda',
+               createGame('brazil','13:00', 'switzerland')) 
+               +
+               createCard('02/12', 'sexta',
+               createGame('cameroon', '16:00','brazil') 
+               + 
+               createGame("south korea", '12:00', "portugal"))
+            
